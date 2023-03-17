76d104d33da27b834b9630f58ff1c1bf98e92837
RsSvc <wonderhays@gmail.com>
2023-03-17 18:22:19 +0200

    removed reads

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,7 @@ use your_bourse_bridge::{setup_and_start, AppContext, SettingsModel, APP_NAME, A
 #[tokio::main]
 async fn main() {
     let (app_name, app_version) = (APP_NAME, APP_VERSION);
-    // let settings = Arc::new(SettingsModel::load(".your-fin").await);
-    let settings = Arc::new(SettingsModel{
-        seq_conn_string: "".to_string(),
-        your_bourse_url: "127.0.0.1:2046".to_string(),
-        your_bourse_sender_company_id: "TopTrader_UAT_Q".to_string(),
-        your_bourse_target_company_id: "YB-UAT2".to_string(),
-        your_bourse_pass: "7bBXk6KLZG".to_string(),
-        liquidity_provider_id: "Yourbourse".to_string(),
-        no_sql_reader: "127.0.0.1:5125".to_string(),
-    });
+    let settings = Arc::new(SettingsModel::load(".your-fin").await);
 
     SeqLogger::enable_from_connection_string(
         APP_NAME.to_string(),
